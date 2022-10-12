c03c6eb3e15b6c876b12b484cc2d7626cfe4c7dc
Brilliantkid87 <brilliantkid87@gmail.com>
2022-10-12 21:12:10 +0700

    Fourth Commit

--- contact.js
+++ contact.js
@@ -1,9 +1,9 @@
 function showData() {
-    let inputName = document.getElementById('input-name').value;
-    let inputEmail = document.getElementById('input-email').value;
-    let inputPhone = document.getElementById('input-phone').value;
-    let inputSubject = document.getElementById('input-subject').value;
-    let inputMessage = document.getElementById('input-messages').value;
+    let inputName = document.getElementById("input-name").value;
+    let inputEmail = document.getElementById("input-email").value;
+    let inputPhone = document.getElementById("input-phone").value;
+    let inputSubject = document.getElementById("input-subject").value;
+    let inputMessage = document.getElementById("input-messages").value;
    
 
     console.log(inputName);
@@ -12,28 +12,26 @@ function showData() {
     console.log(inputSubject);
     console.log(inputMessage);
     
-    if (inputName == '') {
-        return alert('Name is required')
+    if (inputName == "") {
+        return alert("Name is required")
     }
-    if (inputEmail == '') {
-        return alert('email is required')
+    if (inputEmail == "") {
+        return alert("email is required")
     }
-    if (inputPhone == '') {
-        return alert('Phone is required')
+    if (inputPhone == "") {
+        return alert("Phone is required")
     }
-    if (inputSubject == '') {
-        return alert('Subject is required')
+    if (inputSubject == "") {
+        return alert("Subject is required")
     }
-    if (inputMessage == '') {
-        return alert('Message is required')
+    if (inputMessage == "") {
+        return alert("Message is required")
     }
     
     let emailReceiver = "Brilliantkid87@gmail.com"
 
-    let a = document.createElement('a');
+    let a = document.createElement("a");
     a.href = `https://mail.google.com/mail/?view=cm&fs=1&to=${emailReceiver}&su=${inputSubject}&body=hallo nama saya ${inputName} ${inputMessage} silahkan hubungi ${inputPhone}`
 
     a.click()
 }
-
-
